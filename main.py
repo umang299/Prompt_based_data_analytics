c5d62bdf11c8ad3fcb7e8ec096370336e6296791
umang299 <umang91999@gmail.com>
2024-07-01 17:47:06 +0530

    mod: removed dunder code

--- main.py
+++ main.py
@@ -14,32 +14,33 @@ if not os.path.exists(credential_path):
     os.makedirs(os.path.dirname(credential_path), exist_ok=True)
     shutil.copyfile(os.path.join(PROJECT_ROOT, ".streamlit\\credentials.toml"), credential_path)
 
+
 def load_df(csv_path):
     df = pd.read_csv(csv_path)
     df = df.dropna(axis=0)
     df.reset_index(inplace=True)
     return df
 
-if __name__ == '__main__':
-    api_key = os.getenv['OPENAI_API_KEY']
-    if api_key != '':
-        llm = OpenAI(api_token=api_key)
-        csv_file = st.file_uploader(label='Upload excel file.', type=['csv'])
-        if csv_file is not None:
-            df = load_df(csv_path=csv_file)
-            smart_df = pdai.SmartDataframe(df=df, config={"llm": llm})
 
-            tab1, tab2, tab3 = st.tabs(tabs=['Chat', 'Summary', 'Top 10'])
-            with tab1:
-                input = st.text_input(label="Text query")
-                response = smart_df.chat(query=input)
-                if input is not None:
-                    st.text_area(label='Response', value=response)
+api_key = os.getenv['OPENAI_API_KEY']
+if api_key != '':
+    llm = OpenAI(api_token=api_key)
+    csv_file = st.file_uploader(label='Upload excel file.', type=['csv'])
+    if csv_file is not None:
+        df = load_df(csv_path=csv_file)
+        smart_df = pdai.SmartDataframe(df=df, config={"llm": llm})
 
-            with tab2:
-                st.header("Data Description")
-                st.table(df.describe())
-            with tab3:
-                st.table(df.head(10))
-    else:
-        pass
+        tab1, tab2, tab3 = st.tabs(tabs=['Chat', 'Summary', 'Top 10'])
+        with tab1:
+            input = st.text_input(label="Text query")
+            response = smart_df.chat(query=input)
+            if input is not None:
+                st.text_area(label='Response', value=response)
+
+        with tab2:
+            st.header("Data Description")
+            st.table(df.describe())
+        with tab3:
+            st.table(df.head(10))
+else:
+    pass
